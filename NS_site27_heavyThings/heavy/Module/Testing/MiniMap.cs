b0dea8158cd7eb7e2e9fc1e26b81857aa18202f1
killjsj <killjsj@outlook.com>
2026-09-02 19:02:54 +0800

    213

---
+++ NS_site27_heavyThings/heavy/Module/Testing/MiniMap.cs
@@ -0,0 +1,33 @@
+﻿using CommandSystem;
+using MapGeneration;
+using NS_site27_heavy.heavy.Module.testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace NS_site27_heavy.heavy.Module.Testing
+{
+    [CommandHandler(typeof(GameConsoleCommandHandler))]
+    public class PrintMapCommand : ICommand
+    {
+        public string Command => "PMC";
+
+        public string[] Aliases => new[] { "" };
+
+        public string Description => "";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "done!\n";
+            foreach (var item in RoomIdentifier.AllRoomIdentifiers)
+            {
+                if(item == null) continue;
+                response += $"{item}: {item?.Name}({item?.Shape}), {item?.MainCoords},{item?.Icon}({item?.Icon?.name ?? "null"},{item?.Icon?.bounds})\n";
+            }
+            return true;
+        }
+    }
+}
